e257a400ad40d6ff53afb65df3206bccce7b1664
Alex Erofeev <xnet141@gmail.com>
2024-09-26 12:07:26 +0300

    Add Rakefile, update Gemfile, add Product model

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
+
+class Product < ActiveRecord::Base
+end
+
+
 
 get '/' do
 	erb :index
