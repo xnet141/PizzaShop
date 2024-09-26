c4641b344ffa817f6fdfa8d8a6de6d6845444e40
Alex Erofeev <xnet141@gmail.com>
2024-09-26 14:23:10 +0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_26_090912) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_26_102124) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
