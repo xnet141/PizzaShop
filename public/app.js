e5a64a1ce1d7f8faae5c6e4c74943210f6759d22
Alex Erofeev <xnet141@gmail.com>
2024-10-01 17:00:31 +0300

    add cart_get_number_of_items()

--- public/app.js
+++ public/app.js
@@ -9,12 +9,55 @@ function something(id)
 	alert(x);
 }
 
+// ================================================
 
-function addtocart(id)
+function add_to_cart(id)
 {
 	var key = 'product_' + id;
 
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
-	window.localStorage.setItem(key, x)
+	window.localStorage.setItem(key, x);
+	alert(key + ': ' + x + '  Items in your cart: ' + cart_get_number_of_items());
+}
+
+
+function list() // моя работа
+{	
+	var sum = 0
+	for (var i = 1; i <= window.localStorage.length; i++)
+	{
+		console.log("sum: " + sum);
+		var x = window.localStorage.getItem('product_' + i);
+		sum += x * 1;
+		console.log('localStorage.key: ' + localStorage.key(i));
+		console.log("sum: " + sum);
+		console.log(i);
+		console.log(x);
+	}
+	console.log('localStorage.length: ' + localStorage.length);
+	alert(sum);
+}
+
+function test()
+{
+	var q = localStorage.getItem('product_1')
+	alert(q)
+}
+
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+	return cnt;
 }
