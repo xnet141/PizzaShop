0a22fbcf80565401264d12bddf601bb62f4276f7
Alex Erofeev <xnet141@gmail.com>
2024-10-08 01:45:37 +0300

    Home work part 2

--- public/app.js
+++ public/app.js
@@ -9,19 +9,38 @@ function something(id)
 	alert(x);
 }
 
+// function update_orders_button() // общее кол-во пиц на кнопке корзины
+// {
+// 	var text = 'Cart (' + cart_get_orders()[1] + ')';
+// 	$('#orders_button').val(text);
+// }
+
+
+// function cart_get_number_of_items()
+// {
+// 	var cnt = 0;
+// 	for(var i = 0; i < window.localStorage.length; i++)
+// 	{
+// 		var key = window.localStorage.key(i);
+// 		var value = window.localStorage.getItem(key);
+
+// 		if(key.indexOf('product_') == 0)
+// 		{
+// 			cnt = cnt + value * 1;
+// 		}
+// 	}
+// 	return cnt;
+// }
+
+
 // ================================================
 
 
 function update_orders_input() // обновляет поле ввода тега input (скрытое) для отпрааки на сервер
 {
-	var orders = cart_get_orders();
+	var orders = cart_get_orders()[0];
 	$('#orders_input').val(orders);
-}
-
-
-function update_orders_button() // общее кол-во пиц на кнопке корзины
-{
-	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	var text = 'Cart (' + cart_get_orders()[1] + ')';
 	$('#orders_button').val(text);
 }
 
@@ -35,36 +54,14 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 	// alert(key + ': ' + x + '  Items in your cart: ' + cart_get_number_of_items());
 	update_orders_input();
-	update_orders_button();
+	// update_orders_button();
 }
 
 
-function list() // моя работа
-{	
-	var sum = 0
-	for (var i = 1; i <= window.localStorage.length; i++)
-	{
-		console.log("sum: " + sum);
-		var x = window.localStorage.getItem('product_' + i);
-		sum += x * 1;
-		console.log('localStorage.key: ' + localStorage.key(i));
-		console.log("sum: " + sum);
-		console.log(i);
-		console.log(x);
-	}
-	console.log('localStorage.length: ' + localStorage.length);
-	alert(sum);
-}
-
-function test()
+function cart_get_orders()
 {
-	var q = localStorage.getItem('product_1')
-	alert(q)
-}
-
-
-function cart_get_number_of_items()
-{
+	var arr = [];
+	var orders = '';
 	var cnt = 0;
 	for(var i = 0; i < window.localStorage.length; i++)
 	{
@@ -73,35 +70,20 @@ function cart_get_number_of_items()
 
 		if(key.indexOf('product_') == 0)
 		{
+			orders = orders + key + '=' + value + ',';
 			cnt = cnt + value * 1;
 		}
 	}
-	return cnt;
-}
-
-function cart_get_orders()
-{
-	var orders = '';
-	for(var i = 0; i < window.localStorage.length; i++)
-	{
-		var key = window.localStorage.key(i);
-		var value = window.localStorage.getItem(key);
-
-		if(key.indexOf('product_') == 0)
-		{
-			orders = orders + key + '=' + value + ',';
-		}
-	}
-	return orders;
+	arr.push(orders, cnt)
+	return arr;
 }
 
 function cancel_order()
 {
-
 	window.localStorage.clear();
 	update_orders_input();
 	update_orders_button();
 	$('#cart').text('Your cart is now empty. JS - супер!!');
 	return false;
-
 }
+
---
+++ public/test.js
@@ -0,0 +1,10 @@
+
+function test()
+{
+	var a = 4;
+	var b = 10;
+	var arr = [];
+
+	return 4;
+
+}
